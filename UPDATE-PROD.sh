06e906dd0afeae0e9181b1492c5757ab065bf77b
Gary Oberbrunner <garyo@darkstarsystems.com>
2025-11-09 09:49:07 -0500

    Fix update-prod script
    Signed-off-by: Gary Oberbrunner <garyo@darkstarsystems.com>

--- UPDATE-PROD.sh
+++ UPDATE-PROD.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh docker-server 'cd containers/gco-llm-pkm && ./UPDATE-PROD.sh && timeout 30 docker compose logs -f' 
+ssh docker-server 'cd containers/gco-llm-pkm && git pull && docker compose up -d --build && timeout 30 docker compose logs -f' 
